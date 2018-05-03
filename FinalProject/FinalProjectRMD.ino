fc827fcd6e610f9ad5018a0fed22b76aa1512f30
SouLeo <slwanna@utexas.edu>
2018-05-03 14:24:36 -0500

    updated to current file/ includes timing and power settings

--- FinalProject/FinalProjectRMD.ino
+++ FinalProject/FinalProjectRMD.ino
@@ -2,7 +2,7 @@
 
 DualMC33926MotorShield md;
 
-int motor_speed = 225;
+int motor_speed = 200;
 int motor_direction = 0;
 
 int control_switch = 6;
@@ -28,6 +28,15 @@ void setup()
 
 void loop()
 {
+  /*
+   * TODO TEST:
+   * 1. Set angle limits on motor
+   * 2. Make a button start and stop the system
+   * 3. Make a change direction button
+   * 4. PID control on motor speed
+   */
+   
+ /*
   if (Serial.available() > 0){
     if (Serial.read()){
         Serial.println("hi");
@@ -36,4 +45,24 @@ void loop()
       }
   }
   md.setM1Speed(motor_speed);
+*/
+
+ 
+  control_switch_current_status = digitalRead(control_switch);
+  if (control_switch_current_status == HIGH) {
+    Serial.println("Switch Hit");
+    Serial.println("Stopping!");
+    md.setM1Speed(0);
+    delay(100);
+    Serial.println("UP");
+    md.setM1Speed(motor_speed);
+    delay(3000);
+    Serial.println("Stopping!");
+    delay(1000);
+    Serial.println("DOWN");
+    md.setM1Speed(-10);
+    delay(3000);    
+  }
+
+
 }
